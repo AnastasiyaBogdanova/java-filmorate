f79a6276eeb940c8e92c13eec3875cbb26b38fe1
Anastasiya Bogdanova <nastena40480@yandex.ru>
2025-04-08 15:04:21 +0300

    mend

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/InMemoryFilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/InMemoryFilmStorage.java
@@ -81,7 +81,7 @@ public class InMemoryFilmStorage implements FilmStorage {
     @Override
     public List<Film> getPopularFilms(int count) {
         return filmHashMap.values().stream()
-                .sorted(Comparator.comparingInt(film -> film.getUserIdLikes().size()))
+                .sorted(Comparator.comparingLong(film -> film.getUserIdLikes().size()))
                 .limit(count)
                 .toList();
     }
